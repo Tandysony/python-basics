97e2ecfe203feabdf6aab637331fe1da00c1116c
Suo Tan <tandysony@gmail.com>
2017-12-19 16:59:18 -0500

    * Add more contents and a summary in the numbers file;

--- number.py
+++ number.py
@@ -1,10 +1,37 @@
+import sys
+
+# 'int' type
+int_max = sys.maxint
+print("max int in this system is: {} -- {}.".format(int_max, type(int_max)))
+
+# 'long' type
+print(type(int_max+1))
+s_long = 48L
+print("s_long is: {} -- {}".format(s_long, type(s_long)))
+
+# 'float' type
+f = 2.71828182
+print("'f' is: {} -- {}".format(f, type(f)))
+
+# 'complex' type
+cplx = 2.6 +5j
+print("'cplx' is: {} -- {}".format(cplx, type(cplx)))
+print("Its real part is {}, and the imaginary part is {}".format(cplx.real, cplx.imag))
+
 # using ** to calculate powers 
 result = 2 ** 4
 print(result)
 
 # You can assign many varables at once
 one, two, three = 1, "two", 3.0
-print("one = %d, two = %s, three = %f" % (one, two, three))
+print("one = {}, two = {}, three = {}".format(one, two, three))
 
 # round up
 print(round(14.99, 1)) 
+
+
+## SUMMARY
+# 1. There are 3 types of numbers in Python 2.7: int, long, float & complex.
+# 2. 'int' will be converted to 'long' if it overflows
+# 3. Add an 'L' at the end of the int value to enforce a 'long' type
+# 4. User 'x.real' and 'x.imag' to get real part and imaginary part respectively, for a complex number 'x'.
